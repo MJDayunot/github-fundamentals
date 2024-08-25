5cf8fab7fff6894385c1402505d538dc01edbf2b
MJDayunot <mjanedayunot@gmail.com>
2024-08-25 20:01:00 +0800

    commited all variables

---
+++ user_profile.py
@@ -0,0 +1,10 @@
+name = "Mary Jane"
+age = 21
+address = "Caloocan City"
+hobby = "Dancing"  
+
+# Print user information
+print("Name:", name)
+print("Age:", age)
+print("Address:", address)
+print("Hobby:", hobby)
